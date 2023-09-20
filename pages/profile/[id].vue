336cbbf3803e94aa05eeef315e5b3249817304d4
nav-r-c <abhinav.30.kr@gmail.com>
2023-09-20 23:03:35 +0530

    Added Profile Page Head

--- pages/profile/[id].vue
+++ pages/profile/[id].vue
@@ -1,15 +1,22 @@
 <template>
-    <div class = "mx-auto text-center font-[Roboto]">
-        <h1 class="font-bold">Profile Page</h1>
-        <div v-if="userExists">
-            <p>Name: {{ (userDetails.data.value?.data as UserDetails)?.name }}</p>
-            <p>Phone Number: {{ (userDetails.data.value?.data as UserDetails)?.phoneNumber }}</p>
-            <p>DOB: {{ (userDetails.data.value?.data as UserDetails)?.dob }}</p>
-            <p>E-Mail: {{ (userDetails.data.value?.data as UserDetails)?.email }}</p>
+    <div v-if="userExists">
+        <div class = "mx-auto text-center font-[Roboto] text-white">
+            <div class = "bg-[#0A5D00] rounded-b-xl mx-auto w-screen">
+                <img src ="~/public/logo.png" class = "mx-auto py-2 w-[50px]" />
+                <h1 class="font-bold text-white text-4xl my-5 drop-shadow-lg drop-shadow-white">Personal Profile</h1>
+                <img :src = "(userDetails.data.value?.data as UserDetails).image" class = "mx-auto max-w-[500px] w-auto" />
+                <p class = "text-3xl my-2"><span class = 'font-bold'>{{ (userDetails.data.value?.data as UserDetails)?.name?.split(" ")[0] }}</span> <span>{{ (userDetails.data.value?.data as UserDetails)?.name?.split(" ").slice(1).join(" ") }}</span></p>
+                <div class = "py-5 pb-20">
+                    <p class = "text-xl">Membership Status: <span class = "font-bold">{{ (userDetails.data.value?.data as UserDetails)?.membershipStatus }}</span></p>
+                    <p class = "text-xl">Valid from: <span class = "font-bold">{{ (userDetails.data.value?.data as UserDetails)?.validDate }}</span></p>
+                </div>
+                
+            </div>
         </div>
-        <div v-else>
-            <h1>User Does not exist</h1>
-        </div>
+    </div>
+
+    <div v-else>
+        <h1>User Does not exist</h1>
     </div>
 </template>
 
@@ -27,6 +34,8 @@
             name? : string,
             nfcID? : string,
             phoneNumber? : string,
+            image? : string,
+            validDate? : string,
             verified? : boolean
         } | string
     }
@@ -38,6 +47,8 @@
         name? : string,
         nfcID? : string,
         phoneNumber? : string,
+        image? : string,
+        validDate? : string,
         verified? : boolean
     }
 
