971def0ebb904b75a6c69a55091137806518c809
nav-r-c <abhinav.30.kr@gmail.com>
2023-09-21 00:17:29 +0530

    Made Basic Frontend for Profile Page

--- components/DropCard.vue
+++ components/DropCard.vue
@@ -1,3 +1,20 @@
 <template>
-    
+    <div :class = "props.class" class = "bg-white border-2 border-grey rounded-xl my-10 max-w-[400px] mx-auto p-5 font-[Roboto] text-[#1E2968] shadow-md">
+        <h1 class = "font-bold text-xl">{{ props.cardTitle }}</h1>
+        <div class ="py-1">
+            <slot></slot>
+        </div>
+    </div>
 </template>
+
+<script setup lang="ts">
+    interface PropTypes {
+        cardTitle? : string,
+        class? : string
+    }
+
+    const props = withDefaults(defineProps<PropTypes>(), {
+        cardTitle : "Card Title",
+        class : ""
+    })
+</script>
--- pages/profile/[id].vue
+++ pages/profile/[id].vue
@@ -1,10 +1,10 @@
 <template>
     <div v-if="userExists">
         <div class = "mx-auto text-center font-[Roboto] text-white">
-            <div class = "bg-[#0A5D00] rounded-b-xl mx-auto w-screen">
+            <div class = "bg-[#0A5D00] rounded-b-3xl mx-auto">
                 <img src ="~/public/logo.png" class = "mx-auto py-2 w-[50px]" />
                 <h1 class="font-bold text-white text-4xl my-5 drop-shadow-lg drop-shadow-white">Personal Profile</h1>
-                <img :src = "(userDetails.data.value?.data as UserDetails).image" class = "mx-auto max-w-[500px] w-auto" />
+                <img :src = "(userDetails.data.value?.data as UserDetails).image" class = "mx-auto max-w-[400px] w-auto" />
                 <p class = "text-3xl my-2"><span class = 'font-bold'>{{ (userDetails.data.value?.data as UserDetails)?.name?.split(" ")[0] }}</span> <span>{{ (userDetails.data.value?.data as UserDetails)?.name?.split(" ").slice(1).join(" ") }}</span></p>
                 <div class = "py-5 pb-20">
                     <p class = "text-xl">Membership Status: <span class = "font-bold">{{ (userDetails.data.value?.data as UserDetails)?.membershipStatus }}</span></p>
@@ -13,6 +13,36 @@
                 
             </div>
         </div>
+
+        <div class = "-my-20">
+            <DropCard cardTitle = "Personal Information">
+                <div class = "py-2">
+                    <div class = "flex justify-between items-center my-1">
+                        <p>Phone Number:</p>
+                        <p>+91 {{ (userDetails.data.value?.data as UserDetails).phoneNumber?.slice(0, 5) }} {{ (userDetails.data.value?.data as UserDetails).phoneNumber?.slice(5) }}</p>
+                    </div>
+                    <div class = "flex justify-between items-center my-1">
+                        <p>E-Mail Address: </p>
+                        <p>{{ (userDetails.data.value?.data as UserDetails).email }}</p>
+                    </div>
+                    <div class = "flex justify-between items-center my-1">
+                        <p>Date Of Birth: </p>
+                        <p>{{ (userDetails.data.value?.data as UserDetails).dob }}</p>
+                    </div>
+                </div>
+                
+                <hr class = "border-[#A3A1A1]" />
+                <div class = "flex justify-between items-center mt-2">
+                    <h1 class = "font-bold text-xl">Download Certicate </h1>
+                    
+                </div>
+            </DropCard>
+
+            <DropCard cardTitle = "Aadhar Information">
+                <img :src = "(userDetails.data.value?.data as UserDetails).aadhar" class = "rounded-lg border-2 border-grey" />
+            </DropCard>
+        </div>
+
     </div>
 
     <div v-else>
@@ -21,6 +51,7 @@
 </template>
 
 <script setup lang="ts">
+
     const route = useRoute();
     const id = ref(route.params.id);
     const userDetails = await useFetch<UserDetailsResponse>(`/api/userDetails/fetch?tagId=${id.value}`)
@@ -36,7 +67,8 @@
             phoneNumber? : string,
             image? : string,
             validDate? : string,
-            verified? : boolean
+            verified? : boolean,
+            aadhar? : string
         } | string
     }
 
@@ -49,7 +81,8 @@
         phoneNumber? : string,
         image? : string,
         validDate? : string,
-        verified? : boolean
+        verified? : boolean,
+        aadhar? : string
     }
 
     const userExists = computed(() => {
