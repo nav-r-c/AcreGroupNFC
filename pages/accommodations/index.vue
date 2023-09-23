43e311972158b1f024a3cab5704718b79581d9a6
nav-r-c <abhinav.30.kr@gmail.com>
2023-09-23 19:50:15 +0530

    Fixed Design Bugs

--- pages/accommodations/index.vue
+++ pages/accommodations/index.vue
@@ -1,22 +1,25 @@
 <template>
     <div>
         <NuxtLayout name = "main-pages">
-            <div class = "bg-[#0A5D00] py-5 rounded-b-xl pb-[35%]">
-                <img src = "/logo-whitebg.png" class = "mx-auto" alt = "logo"/>
-                <h1 class = "text-white drop-shadow-lg font-bold text-center font-[Roboto] text-3xl mb-10">Accommodations</h1> 
-            </div>
+            <div class = "mb-[280px]">
+                <div class = "bg-[#0A5D00] py-5 rounded-b-xl pb-[35%]">
+                    <img src = "/logo-whitebg.png" class = "mx-auto" alt = "logo"/>
+                    <h1 class = "text-white drop-shadow-lg font-bold text-center font-[Roboto] text-3xl mb-10">Accommodations</h1> 
+                </div>
 
-            <div class = "-my-[40%]">
-                <div v-for = "acc in (accsList.data.value?.data as Accs[])">
-                    <ImageCard 
-                        :description="acc.description"
-                        :title="acc.title"
-                        :location="acc.location"
-                        :image-link="acc.imageLink"
-                        :mapLink="acc.mapLink"
-                    />
+                <div class = "-my-[45%]">
+                    <div v-for = "acc in (accsList.data.value?.data as Accs[])">
+                        <ImageCard 
+                            :description="acc.description"
+                            :title="acc.title"
+                            :location="acc.location"
+                            :image-link="acc.imageLink"
+                            :mapLink="acc.mapLink"
+                        />
+                    </div>
                 </div>
             </div>
+
         </NuxtLayout>
     </div>
 </template>
